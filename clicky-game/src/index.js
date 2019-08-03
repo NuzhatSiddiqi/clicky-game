a9fba825bbc6b79f8c7c356fe3f9f8969721ed60
Nuzhat Siddiqi <46729206+NuzhatSiddiqi@users.noreply.github.com>
2019-08-03 12:48:09 -0400

    update

--- clicky-game/src/index.js
+++ clicky-game/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
@@ -6,4 +6,12 @@ import Header from './components/header/header';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Header />, document.getElementById('root'));
-registerServiceWorker();
+registerServiceWorker();*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+//renders the App.js file to the root element (index.html in public folder)
+ReactDOM.render(<App />, document.getElementById("root"));
